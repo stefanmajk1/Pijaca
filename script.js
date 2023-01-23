2710287e8c3ef1663f4f97503e201817dd67a817
stefanmajk1 <stefanmajkic00@gmail.com>
2023-01-23 10:28:22 +0100

    pijaca
    vezba o pijaci

---
+++ script.js
@@ -0,0 +1,57 @@
+let allTotal = 0;
+
+function addToCart(element){
+    let mainEl = element.closest('.single-item');
+    let price = mainEl.querySelector('.price').innerText;
+    let name = mainEl.querySelector('h3').innerText;
+    let quantity = mainEl.querySelector('input').value;
+    let cartItems = document.querySelector('.cart-items');
+
+
+    if(parseInt(quantity)> 0){
+
+        price = price.substring(1);
+        price = parseInt(price);
+        quantity = parseInt(quantity);
+        let total = price * quantity;
+        allTotal +=total;
+        cartItems.innerHTML += `<div class="cart-single-item">
+                                <h3>${name}</h3>
+                                <p>${price} x ${quantity} = $<span>${total}</span></p>
+                                <button class="remove-item" onclick="removeFromCart(this)">Ukloni</button>
+                                </div>`;
+        document.querySelector('.total').innerText = `Total: $${allTotal}`;
+
+        element.innerText = "Dodato";
+        element.setAttribute('disabled', 'true');
+        mainEl.querySelector('input').setAttribute("readonly", true);
+    }else{
+        alert('Odaberi kolicinu!');
+    }
+
+}
+
+function removeFromCart(element){
+    console.log('radi');
+    let mainEl = element.closest('.cart-single-item');
+    let total = mainEl.querySelector('p span').innerText;
+    let name = mainEl.querySelector('h3').innerText;
+    let vegetables = document.querySelectorAll('.single-item');
+    total = parseInt(total);
+
+    allTotal -=total;
+    document.querySelector('.total').innerHTML = `Total: $${allTotal}`;
+
+    mainEl.remove();
+
+    vegetables.forEach(function (vege){
+        let itemName = vege.querySelector('.si-content h3').innerText;
+        if(itemName === name){
+            vege.querySelector('.actions input').value = 0;
+            vege.querySelector('.actions input').setAttribute('readonly', false);
+            vege.querySelector('.actions button').removeAttribute('disabled');
+            vege.querySelector('.actions button').innerText = "Dodaj";
+        }
+    });
+
+}
